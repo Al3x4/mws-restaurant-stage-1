30f82ca023b6e0d38d2aad597a4207b4abff8acd
Alexandra Ralev <alexandrapredaralev@gmail.com>
2018-06-14 23:12:07 +0100

    fix: add MAPBOX key

--- js/restaurant_info.js
+++ js/restaurant_info.js
@@ -22,7 +22,7 @@ initMap = () => {
         scrollWheelZoom: false
       });
       L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.jpg70?access_token={mapboxToken}', {
-        mapboxToken: '<your MAPBOX API KEY HERE>',
+        mapboxToken: 'pk.eyJ1IjoiYWxleHByIiwiYSI6ImNqaWYyd29vODBndW0zdm43d3htMjh3a24ifQ.0wwbeKbehnCe37XVVDh--Q',
         maxZoom: 18,
         attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, ' +
           '<a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
